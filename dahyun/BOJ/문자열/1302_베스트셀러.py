06d6d663d5112f7004e1e5bb69b5a9b98ca7977c
Lee Dahyun <75965656+dahyun0917@users.noreply.github.com>
2023-02-06 19:42:51 +0900

    [BOJ] 1주차 1302_베스트셀러.py

--- dahyun/BOJ/문자열/1302_베스트셀러.py
+++ dahyun/BOJ/문자열/1302_베스트셀러.py
@@ -8,4 +8,4 @@ for _ in range(int(input())):  # 입력받은 책의 개수만큼 반복문
     else : dic[text]=1       # 존재하지 않으면 사전에 해당 책 제목 추가하기
 
 # 해당 사전의 items의 values를 기준으로 내림차순 , keys를 기준으로 오름차순 정렬 후, 첫 번째 값 출력
-print(sorted(dic.items(),key=lambda item : (-item[1],item[0]))[0][0])력
+print(sorted(dic.items(),key=lambda item : (-item[1],item[0]))[0][0])
