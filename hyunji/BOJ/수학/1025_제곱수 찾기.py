1911b5c1339e99184d3324ec2b56ad7894bfc17c
hynji <74905604+Shinhyunji-hash@users.noreply.github.com>
2023-02-15 13:34:37 +0900

    Create 1025_제곱수 찾기.py

---
+++ hyunji/BOJ/수학/1025_제곱수 찾기.py
@@ -0,0 +1,25 @@
+#블로그 참조
+from math import sqrt
+N,M=map(int,input().split())
+
+arr=[input().rstrip() for _ in range(N)]
+
+result=-1
+
+for i in range(N): #시작 행
+    for j in range(M): #시작 열
+        for a in range(-N,N): #행 등차
+            for b in range(-M,M): #열 등차
+                
+                num=''
+                y,x=i,j
+                
+                while 0<=y<N and 0<=x<M: 
+                    num+=arr[y][x]
+                    if a==0 and b==0:
+                        break # 둘 다 공차가 0이면 break 빠져나옴
+                    if int(sqrt(int(num)))**2==int(num): #제곱수인지 판별
+                        result=max(int(num),result)
+                    y+=a
+                    x+=b
+print(result)
