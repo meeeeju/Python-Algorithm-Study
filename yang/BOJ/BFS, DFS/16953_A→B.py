9189a06d749065bbceff79496da53d776174b5d3
yang1318 <amy1353@naver.com>
2023-02-19 03:42:55 +0900

    [BOJ] 3주차 16953_ A→B.py

---
+++ yang/BOJ/BFS, DFS/16953_A→B.py
@@ -0,0 +1,15 @@
+A, B = map(int, input().split())
+count = 0
+while A < B:
+    if B%10 == 1:
+        B = B//10
+    elif B%2 == 0:
+        B = B//2
+    else:
+        break
+    count += 1
+
+if A == B:
+    print(count+1)
+else:
+    print(-1)
