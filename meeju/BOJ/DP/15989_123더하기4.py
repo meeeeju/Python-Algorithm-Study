1e954c428687cfcad757847900c45253a3698ed2
meeju <68631158+meeeeju@users.noreply.github.com>
2023-03-09 20:05:14 +0900

    Update 15989_123더하기4.py

--- meeju/BOJ/DP/15989_123더하기4.py
+++ meeju/BOJ/DP/15989_123더하기4.py
@@ -1,49 +1,27 @@
+# 31256KB /	44ms
+# 블로그 참고 / 질문 게시판 참고
+
+
 import sys
 input = sys.stdin.readline
 
-
-'''
-3 # 테스트 케이스
-4
-7
-10'''
-
-def number_of_method(a):
-    return a
-
 T = int(input())
 test_num = [None for _ in range(T)]
-
 for i in range(T):
     test_num[i]= int(input())
-    #print(number_of_method(int(input())))
 
-max_num = max(test_num)
-dp = [ 0 for _ in range(test_num+1)]
-dp[1]=1 # (1)
-dp[2]=1 # (1+1)
-dp[3]=2 # (1+1+1, 1+2)
-dp[4]=4
-dp[5]=5
-dp[6]=7
+max_num = max(test_num)     # 테스트 케이스 중에서 가장 큰 수
+
+dp= [1 for _ in range(max_num+1)]     # 모든 숫자는 1로만 더해지는 방법을 다 가지고 있으니까 1로 초기화시키기
 
 
-for i in range(7,max+1):
-    a= dp[i-1]
-    if i-2 ==3 or i-2 ==2:
-        b= 1
-    else:
-        b= dp[i-2] -dp[i-3]
-    if 
-        
-        or i-2 ==3:
+for i in range(2,max_num+1):    # 2를 적어도 하나를 포함하고 있는 모든 경우의 수 더해주기 (1,2 로 구성된 조합)
+    dp[i] += dp[i-2]
 
-    b= dp[i-2]-dp[i-3]
-    c= dp[i-3]-(dp[i-4]+dp[i-5])
+for i in range(3,max_num+1):     # 3을 적어도 하나를 포함하고 있는 모든 경우의 수 더해주기 (1,2,3 으로 구성된 조합)
+    dp[i] += dp[i-3]
+    
 
 
-
-
-
-
-
+for i in test_num:
+    print(dp[i])
