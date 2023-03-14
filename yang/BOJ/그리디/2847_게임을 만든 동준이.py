ed55f138e5d5fa7514dd67b4accca515401a2382
yang1318 <amy1353@naver.com>
2023-03-15 01:29:26 +0900

    [BOJ] 6주차 2847_게임을 만든 동준이.py

--- yang/BOJ/그리디/2847_게임을 만든 동준이.py
+++ yang/BOJ/그리디/2847_게임을 만든 동준이.py
@@ -1,16 +1,17 @@
-# 31256KB / 40ms
+# 31256KB / 44ms
 import sys
 input = sys.stdin.readline
 n = int(input())
 score = [0]
 ans = 0
-for i in range(1, n+1):
-    score.append(int(input()))
-    if score[i-1] >= score[i]:
-        for k in range(i, 0, -1): # i~1
-            if score[k-1] >= score[k]:
-                ans += score[k-1]-score[k]+1
-                score[k-1] = score[k]-1
+# for i in range(1, n+1):
+#     score.append(int(input()))
+#     if score[i-1] >= score[i]:
+#         for k in range(i, 0, -1): # i~1
+#             if score[k-1] >= score[k]:
+#                 ans += score[k-1]-score[k]+1
+#                 score[k-1] = score[k]-1
+
 # for i in range(n):
 #     score.append(int(input()))
 # for i in range(n, 0, -1): # n~1
@@ -19,4 +20,11 @@ for i in range(1, n+1):
 #             if score[k-1] >= score[k]:
 #                 ans += score[k-1]-score[k]+1
 #                 score[k-1] = score[k]-1
+
+for i in range(n):
+    score.append(int(input()))
+for i in range(n, 0, -1): # n~1
+    if score[i-1] >= score[i]:
+        ans += score[i-1]-score[i]+1
+        score[i-1] = score[i]-1
 print(ans)
