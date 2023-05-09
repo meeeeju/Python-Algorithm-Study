db326dcea5f76dd704f0fc968c4b43bcf7bf11df
Lee Dahyun <75965656+dahyun0917@users.noreply.github.com>
2023-05-09 17:14:51 +0900

    [BOJ] 13주차 12026_BOJ 거리.py

--- dahyun/BOJ/DP/12026_BOJ 거리.py
+++ dahyun/BOJ/DP/12026_BOJ 거리.py
@@ -4,7 +4,7 @@ import sys
 input=sys.stdin.readline
 N=int(input())
 nlist=list(map(str,input().strip()))
-dp=[999999999 for _ in range(N)]인
+dp=[999999999 for _ in range(N)]
 dp[0]=0
 for i in range(1,N):
     for j in range(i): # 그 자리를 기준의 최솟값을 앞에서부터 확
