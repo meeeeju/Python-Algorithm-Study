c9c7f317d162828cc7d4043f5c5ff62a9334fcd5
Lee Dahyun <75965656+dahyun0917@users.noreply.github.com>
2023-05-09 17:15:09 +0900

    [BOJ] 13주차 12026_BOJ 거리.py

--- dahyun/BOJ/DP/12026_BOJ 거리.py
+++ dahyun/BOJ/DP/12026_BOJ 거리.py
@@ -7,7 +7,7 @@ nlist=list(map(str,input().strip()))
 dp=[999999999 for _ in range(N)]
 dp[0]=0
 for i in range(1,N):
-    for j in range(i): # 그 자리를 기준의 최솟값을 앞에서부터 확
+    for j in range(i): # 그 자리를 기준의 최솟값을 앞에서부터 확인
         if (nlist[i]=='O' and nlist[j]=='B') or (nlist[i]=='J' and nlist[j]=='O') or (nlist[i]=='B' and nlist[j]=='J'):
             dp[i]=min(dp[i],dp[j]+pow(i-j,2))
 print(dp[N - 1] if dp[N - 1] != 999999999 else -1)
