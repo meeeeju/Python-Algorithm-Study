5dfce0d514ad861fb768cdb6d9c0f176baf4e5d5
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-05-12 07:25:09 +0900

    [BOJ] 13주차 2217_로프.py

---
+++ yang/BOJ/그리디/2217_로프.py
@@ -0,0 +1,13 @@
+# 35500KB / 124ms
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lope = []
+for _ in range(n):
+    lope.append(int(input()))
+lope.sort(reverse=True)
+ans = lope[0]
+for i in range(n):
+    ans = max(ans, lope[i]*(i+1))
+print(ans)
