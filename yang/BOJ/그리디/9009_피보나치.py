a58648e97eb3b5eb110c21c3e5f0996ee70998e3
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-05-17 17:11:00 +0900

    [BOJ] 14주차 9009_피보나치.py

---
+++ yang/BOJ/그리디/9009_피보나치.py
@@ -0,0 +1,18 @@
+# 블로그참고
+p = [1,2]
+for i in range(2, 46):
+    p.append(p[i-2]+p[i-1]) 
+T = int(input())
+
+for j in range(T):
+    n = int(input())
+    result=[]
+    while(n):
+        for k in range(46):
+            if(p[k]<=n):
+                t = p[k]
+        n -= t
+        result.append(t)
+        result.sort()
+    for b in range(len(result)):
+        print(result[b], end=' ')
