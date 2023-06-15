aefe927d4b093b9bbee606458389df22aa360d45
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-06-15 14:25:22 +0900

    [BOJ] 19주차 9095_1, 2, 3 더하기.py

---
+++ yang/BOJ/DP/9095_1, 2, 3 더하기.py
@@ -0,0 +1,16 @@
+# 31256KB / 40ms
+import sys
+input = sys.stdin.readline
+
+dp = [0]*12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in range(4, 12):
+    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+
+T = int(input())
+for _ in range(T):
+    num = int(input())
+    print(dp[num])
