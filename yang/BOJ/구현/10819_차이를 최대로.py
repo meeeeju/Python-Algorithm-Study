f1f03fc716babd77ff43ecf00c84327b9b8b8698
yang1318 <amy1353@naver.com>
2023-06-25 23:41:33 +0900

    [BOJ] 20주차 10819_차이를 최대로.py

---
+++ yang/BOJ/구현/10819_차이를 최대로.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def find(turn):
+    if turn == N:
+        global answer
+        test = 0
+        temp = array[debug[0]]
+        for i in range(1, N):
+            temp -= array[debug[i]]
+            test += abs(temp)
+            temp = array[debug[i]]
+        answer = max(answer, test)
+        # print(test, debug)
+        return
+    
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            debug.append(i)
+            find(turn+1)
+            debug.pop()
+            visited[i] = False
+    return
+
+N = int(input())
+array = list(map(int, input().split()))
+answer = 0
+visited = [False]*N
+debug = []
+find(0)
+print(answer)
