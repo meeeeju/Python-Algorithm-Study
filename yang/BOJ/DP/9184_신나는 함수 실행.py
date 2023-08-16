2fef3f8e479006483b823d6a8e13614e17491c5f
seona.Yang <75965560+yang1318@users.noreply.github.com>
2023-08-16 18:12:32 +0900

    [BOJ] 27주차 9184_신나는 함수 실행.py

---
+++ yang/BOJ/DP/9184_신나는 함수 실행.py
@@ -0,0 +1,26 @@
+# 31256KB / 68ms
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+def w(a, b, c):
+    if a<=0 or b<=0 or c<=0:
+        return 1
+    if a>20 or b>20 or c>20:
+        return w(20, 20, 20)
+    if dp[a][b][c]!='*':
+        return dp[a][b][c]
+    if a<b<c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+    return dp[a][b][c]
+
+size = 21
+dp = [[['*']*size for _ in range(size)] for _ in range(size)] # dp[a][b][c]
+while True:
+    a, b, c = map(int, input().split())
+    if a==-1 and b==-1 and c==-1: break
+    if a<=0 or b<=0 or c<=0: ans = 1
+    else: ans = w(a, b, c)
+    print(f"w({a}, {b}, {c}) = {ans}")
